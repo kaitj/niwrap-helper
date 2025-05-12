ee4cd5dbfbefd2c334d32b7fd813bddd35b5794d
Jason Kai <21226986+kaitj@users.noreply.github.com>
2025-05-12 14:46:51 -0400

    Switch to b2t2
    - Also update miniminum python versions required
    
    NOTE: This update is not packwards compatible - packages using old b2t should be pinned

--- src/niwrap_helper/bids.py
+++ src/niwrap_helper/bids.py
@@ -3,28 +3,33 @@
 from pathlib import Path
 from typing import Literal, overload
 
+import bids2table as b2t
 import pandas as pd
-from bids2table import BIDSEntities, bids2table
+import pyarrow as pa
+import pyarrow.parquet as pq
 
 from niwrap_helper.types import StrPath
 
 
 def get_bids_table(
-    dataset_dir: StrPath, dataset_index: str = ".index.b2t", workers: int = 1
+    dataset_dir: StrPath,
+    index: StrPath = ".index.b2t",
 ) -> pd.DataFrame:
     """Get and return BIDSTable for a given dataset."""
     dataset_dir = Path(dataset_dir)
-    b2t = bids2table(
-        root=dataset_dir,
-        index_path=dataset_dir / dataset_index,
-        with_meta=False,
-        workers=workers,
-    )
-    extra_entities = pd.json_normalize(b2t["ent__extra_entities"]).set_index(b2t.index)  # type: ignore
-    b2t = pd.concat([b2t, extra_entities.add_prefix("ent__")], axis=1).drop(
-        columns="ent__extra_entities"
-    )
-    return b2t
+
+    # Get table
+    if (index_fp := (dataset_dir / index)).exists():
+        table: pa.Table = pq.read_table(index_fp)
+    else:
+        tables = b2t.batch_index_dataset(b2t.find_bids_datasets(dataset_dir))
+        table: pa.Table = pa.concat_tables(tables)  # type: ignore[no-redef]
+
+    # Normalize extra entities column
+    extra_entities_df = pd.json_normalize(table.column("extra_entities").to_pylist())
+    table = table.drop(["extra_entities"])
+
+    return pd.concat([table.to_pandas(), extra_entities_df], axis=1)
 
 
 @overload
@@ -51,5 +56,5 @@ def bids_path(
     """Generate BIDS name / path."""
     if directory and return_path:
         raise ValueError("Only one of 'directory' or 'return_path' can be True")
-    name = BIDSEntities.from_dict(entities).to_path()
+    name = b2t.format_bids_path(entities)
     return name.parent if directory else name if return_path else name.name
