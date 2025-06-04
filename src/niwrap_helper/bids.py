b79af8c6313ce3806c3d1ff17328b032be5c5d1d
Jason Kai <21226986+kaitj@users.noreply.github.com>
2025-06-04 14:37:25 -0400

    Only retain support for pyarrow
    This drops support within this package for pandas. This also allows external packages to use
    pandas or pandas as desired.

--- src/niwrap_helper/bids.py
+++ src/niwrap_helper/bids.py
@@ -6,39 +6,26 @@ import bids2table as b2t
 import pyarrow as pa
 import pyarrow.parquet as pq
 from bids2table._pathlib import PathT, as_path
-from pandas import DataFrame
 
 from niwrap_helper.types import StrPath
-
-
-@overload
-def get_bids_table(
-    dataset_dir: StrPath, index: StrPath | None, return_type: Literal["pandas"]
-) -> DataFrame: ...
-
-
-@overload
-def get_bids_table(
-    dataset_dir: StrPath, index: StrPath | None, return_type: Literal["pyarrow"]
-) -> pa.Table: ...
 
 
 def get_bids_table(
     dataset_dir: StrPath,
     index: StrPath | None = ".index.b2t",
-    return_type: Literal["pandas"] | Literal["pyarrow"] = "pandas",
-) -> pa.Table | DataFrame:
+) -> pa.Table:
     """Get and return BIDSTable for a given dataset."""
     dataset_dir = as_path(dataset_dir)
 
-    # Get table
-    if index is not None and (index_fp := (dataset_dir / index)).exists():
+    # Load / generate table
+    index_fp = dataset_dir / index if index else None
+    if index_fp and index_fp.exists():
         table = pq.read_table(index_fp)
     else:
         tables = b2t.batch_index_dataset(b2t.find_bids_datasets(dataset_dir))  # type: ignore
         table = pa.concat_tables(tables)
 
-    # Set each extra entity as separate column, if it exists
+    # Expand 'extra_entities' into columns
     if "extra_entities" in table.column_names:
         extra_entities = table.column("extra_entities").to_pylist()
         extra_entities_dicts = [
@@ -54,13 +41,10 @@ def get_bids_table(
             table = pa.concat_tables(
                 [table, extra_entities_table], promote_options="default"
             )
+
         table = table.drop(["extra_entities"])
 
-    dispatch = {"pandas": table.to_pandas, "pyarrow": lambda: table}
-    try:
-        return dispatch[return_type]()
-    except KeyError:
-        raise ValueError(f"Unsupported return_type: {return_type}")
+    return table
 
 
 @overload
