704b3b09fadc600e42d8d2f4a661f9a93d1bd081
Jason Kai <21226986+kaitj@users.noreply.github.com>
2025-08-27 16:00:37 -0400

    Make setup_styx more robust, update types

--- src/niwrap_helper/styx.py
+++ src/niwrap_helper/styx.py
@@ -4,90 +4,124 @@ import logging
 import os
 import shutil
 from pathlib import Path
-from typing import overload
+from typing import Literal, overload
 
 import yaml
-from styxdefs import LocalRunner, OutputPathType, set_global_runner
-from styxdocker import DockerRunner
-from styxsingularity import SingularityRunner
+from niwrap import (
+    DockerRunner,
+    GraphRunner,
+    LocalRunner,
+    SingularityRunner,
+    set_global_runner,
+)
 
 from niwrap_helper.types import (
+    BaseRunner,
     DockerType,
     LocalType,
     SingularityType,
     StrPath,
-    StyxRunner,
 )
 
 
 @overload
+def setup_styx() -> tuple[logging.Logger, LocalRunner]: ...
+
+
+@overload
 def setup_styx(
-    image_map: StrPath | None,
     runner: DockerType,
-    tmp_env: str = "LOCAL",
-    tmp_dir: str = "styx_tmp",
+    tmp_env: str,
+    tmp_dir: str,
+    image_map: StrPath | dict[str, str] | None,
+    graph_runner: Literal[False],
 ) -> tuple[logging.Logger, DockerRunner]: ...
 
 
 @overload
 def setup_styx(
-    image_map: StrPath | None,
     runner: SingularityType,
-    tmp_env: str = "LOCAL",
-    tmp_dir: str = "styx_tmp",
+    tmp_env: str,
+    tmp_dir: str,
+    image_map: StrPath | dict[str, str] | None,
+    graph_runner: Literal[False],
 ) -> tuple[logging.Logger, SingularityRunner]: ...
 
 
 @overload
 def setup_styx(
-    image_map: StrPath | None,
     runner: LocalType,
-    tmp_env: str = "LOCAL",
-    tmp_dir: str = "styx_tmp",
+    tmp_env: str,
+    tmp_dir: str,
+    image_map: StrPath | dict[str, str] | None,
+    graph_runner: Literal[False],
 ) -> tuple[logging.Logger, LocalRunner]: ...
 
 
+@overload
 def setup_styx(
-    image_map: StrPath | None = None,
+    runner: str,
+    tmp_env: str,
+    tmp_dir: str,
+    image_map: StrPath | dict[str, str] | None,
+    graph_runner: Literal[True],
+) -> tuple[logging.Logger, GraphRunner]: ...
+
+
+def setup_styx(
     runner: str = "local",
     tmp_env: str = "LOCAL",
     tmp_dir: str = "styx_tmp",
-) -> tuple[logging.Logger, StyxRunner]:
+    image_map: StrPath | dict[str, str] | None = None,
+    graph_runner: bool = False,
+) -> tuple[logging.Logger, BaseRunner | GraphRunner]:
     """Setup Styx runner."""
-    runner = runner.lower()
-    if runner == "docker":
-        styx_runner = DockerRunner()
-    elif runner == "singularity" or runner == "apptainer":
-        if image_map is None:
-            raise ValueError("No container mapping provided")
-        styx_runner = SingularityRunner(
-            images=yaml.safe_load(Path(image_map).read_text())
-        )
-    else:
-        styx_runner = LocalRunner()
-    styx_runner.data_dir = Path(os.getenv(tmp_env, "/tmp")) / tmp_dir
+    match runner.lower():
+        case "docker":
+            styx_runner = DockerRunner()
+        case "singularity" | "apptainer":
+            if isinstance(image_map, (str, Path)):
+                styx_runner = SingularityRunner(
+                    images=yaml.safe_load(Path(image_map).read_text())
+                )
+            elif isinstance(image_map, dict):
+                styx_runner = SingularityRunner(images=image_map)
+            else:
+                raise ValueError("No container mapping provided")
+        case _:
+            styx_runner = LocalRunner()
 
+    logger_name = styx_runner.logger_name
+    styx_runner.data_dir = Path(os.getenv(tmp_env, "/tmp")) / tmp_dir
+    if graph_runner:
+        styx_runner = GraphRunner(styx_runner)
     set_global_runner(styx_runner)
 
-    return logging.getLogger(styx_runner.logger_name), styx_runner
+    return logging.getLogger(logger_name), styx_runner
 
 
-def gen_hash(runner: StyxRunner) -> str:
+def _get_base_runner(runner: BaseRunner | GraphRunner) -> BaseRunner:
+    """Return base styx runner used."""
+    if isinstance(runner, GraphRunner):
+        return runner.base
+    return runner
+
+
+def gen_hash(runner: BaseRunner | GraphRunner) -> str:
     """Generate hash for styx runner."""
-    runner.execution_counter += 1
-    return f"{runner.uid}_{runner.execution_counter - 1}"
+    base_runner = _get_base_runner(runner=runner)
+    base_runner.execution_counter += 1
+    return f"{base_runner.uid}_{base_runner.execution_counter - 1}"
 
 
-def cleanup(runner: StyxRunner) -> None:
+def cleanup(runner: BaseRunner | GraphRunner) -> None:
     """Clean up after completing run."""
-    shutil.rmtree(runner.data_dir)
-    runner.execution_counter = 0
+    base_runner = _get_base_runner(runner=runner)
+    base_runner.execution_counter = 0
+    shutil.rmtree(base_runner.data_dir)
 
 
-def save(
-    files: OutputPathType | list[OutputPathType],
-    out_dir: Path,
-) -> None:
+def save(files: Path | list[Path], out_dir: Path) -> None:
     """Save file(s) to specified output directory, preserving directory structure."""
 
     def _save_file(fpath: Path) -> None:
